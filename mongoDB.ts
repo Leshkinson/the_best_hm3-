142e126ba73d364d4c56a8df659b9ada27e36aba
Dmitri Aleks <alexeyonok.dmitriy@neatsoft.io>
2023-03-15 11:12:30 +0300

    TEST

--- mongoDB.ts
+++ mongoDB.ts
@@ -4,7 +4,7 @@ import {BlogType, PostType} from "./src/types";
 
 dotenv.config()
 
-const mongoURi = process.env.MONGO_URL  || "mongodb://127.0.0.1:27017"
+const mongoURi = process.env.MONGO_URL  || "mongodb+srv://dimaaleks943:AdB7QJrdw9zyOLUY@cluster0.kv7bgcj.mongodb.net/?retryWrites=true&w=majority"
 const client =  new MongoClient(mongoURi)
 const myDB = process.env.npm_lifecycle_script !== "jest"  ?  client.db("myDB") : client.db("myTest")
 export const postCollections = myDB.collection<PostType>("posts")
